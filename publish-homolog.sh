f1398ea821ae2fe82f410d6b78c967af0f0b661e
Cleiton Marques <cleiton.p.marques@gmail.com>
2022-07-07 20:21:41 +0100

    Backup before wipe directory

--- publish-homolog.sh
+++ publish-homolog.sh
@@ -1,3 +1,4 @@
 ng build --configuration=homolog
 ssh-add ~/.ssh/cleiton/id_rsa
-rsync -azP --delete ./public/* dokku-prod:/storage/dokku/homolog-sglegis/public
+ssh -t cleiton@dokku-prod -- "sh -c '~/backup-files.sh'"
+rsync -azP ./public/* dokku-prod:/storage/dokku/homolog-sglegis/public
